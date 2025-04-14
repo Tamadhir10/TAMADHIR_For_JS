be6ff12a125375f68b25da2696f169d9c9bf78cd
Tamadhir10 <tamadhir.alhabsi@gmail.com>
2025-04-14 11:27:14 +0400

    including the fizzBuzz by using loop and if statement

--- Basic_JavaScript_practice/Script.js
+++ Basic_JavaScript_practice/Script.js
@@ -21,7 +21,21 @@ const check =() => {
     }
     sumResult.innerHTML = "Sum of numbers from 1 to " + n + " is: " + sum;
 
-    
+    //FizzBuzz
+
+for (let s = 1; s <= 100; s++) {
+    if (s % 3 === 0 && s % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (s % 3 === 0) {
+        console.log("Fizz");
+    } else if (s % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(s);
+    }
+
+}   
+
 
 
 }
